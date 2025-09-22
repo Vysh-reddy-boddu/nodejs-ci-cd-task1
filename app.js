499e54ffc5c8b3651c0860532482f47c97abd84b
Vyshreddy <bodduvyshnavi77@gmail.com>
2025-09-22 08:01:10 +0000

    Update app message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js CI/CD Demo!');
+  res.send('Updates Demo!');
 });
 
 app.listen(port, () => {
